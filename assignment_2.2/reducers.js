ef5992be3acb2aff35fbdabb5231539594fb5ced
git-ginwook <leeginw@oregonstate.edu>
2022-07-04 12:58:35 -0700

    assignment 2.2 base

---
+++ assignment_2.2/reducers.js
@@ -0,0 +1,19 @@
+'use strict';
+// Don't add or change anything above this comment.
+
+/**
+ * See assignment description for the requirements
+ */
+function reducer1(previousValue, currentValue) {
+   //  Write your code here
+};
+
+/**
+ * See assignment description for the requirements
+ */
+function reducer2(previousValue, currentValue){
+    //  Write your code here
+};
+
+// Don't add or change anything below this comment.
+module.exports = { reducer1, reducer2 };
