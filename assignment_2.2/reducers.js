62cdfbf7f69119080abb043a362f0ffadb05bada
git-ginwook <leeginw@oregonstate.edu>
2022-07-04 13:58:58 -0700

    assignment 2.2 complete

--- assignment_2.2/reducers.js
+++ assignment_2.2/reducers.js
@@ -1,18 +1,40 @@
 'use strict';
 // Don't add or change anything above this comment.
 
+// ONID: leeginw
+
 /**
  * See assignment description for the requirements
  */
 function reducer1(previousValue, currentValue) {
-   //  Write your code here
+    // check whether previousValue is a number
+    if(typeof(previousValue) !== 'number'){
+        previousValue = 0
+    }
+
+    // check whether currentValue is a number
+    if(typeof(currentValue) !== 'number'){
+        currentValue = 0
+    }
+
+    return previousValue + currentValue;
 };
 
 /**
  * See assignment description for the requirements
  */
 function reducer2(previousValue, currentValue){
-    //  Write your code here
+    // throw TypeError if previousValue is not a number
+    if(typeof(previousValue) !== 'number'){
+        throw TypeError();
+    }
+
+    // throw TypeError if currentValue is not a number
+    if(typeof(currentValue) !== 'number'){
+        throw TypeError();
+    }
+
+    return previousValue + currentValue;
 };
 
 // Don't add or change anything below this comment.
