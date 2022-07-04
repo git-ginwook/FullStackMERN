56a7eaa21f5edec59fbdc789548745de12888996
git-ginwook <leeginw@oregonstate.edu>
2022-07-03 18:15:39 -0700

    assignment 2.1 complete (ver.2)

--- assignment_2/deepEqual.js
+++ assignment_2/deepEqual.js
@@ -7,123 +7,64 @@
 * Don't change the declaration of this function.
 */
 function deepEqual(val1, val2) {
-    // check whether each argument is a null 
-    if(val1 === null && val2 !== null){
-        return false;
-    }
-    if(val1 !== null && val2 === null){
-        return false;
-    }
-    
-    // case 3
-    if(typeof(val1) === 'object' && typeof(val2) !== 'object'){
-        return false;
-    }
-    if(typeof(val1) !== 'object' && typeof(val2) === 'object'){
-        return false;
-    }
+    // both are primitives
+    if(typeof(val1) !== 'object' && typeof(val2) !== 'object'){
+        // check whether the two values are strictly equal
+        if(val1 === val2){
+            return true;
+        } else{
+            return false;
+        }
 
-    // case 1, case 2, & case 4
-    if(val1 === val2){
-        return true;
+    // both are non-primitives (objects, arrays, null)
+    } else if(typeof(val1) === 'object' && typeof(val2) === 'object'){
+        // both are nulls
+        if(val1 === null && val2 === null){
+            return true;
 
-    } else{    
-        // case 5
-        if(typeof(val1) === 'object' && typeof(val2) === 'object'){
-            // case 5.1
-            if(Array.isArray(val1) === false && Array.isArray(val2) === false){
-                // object length check
-                if(Object.keys(val1).length !== Object.keys(val2).length){
-                    return false;
-                }
-                
-                // call recursive as needed
-                if(recurObj(val1, val2) === false){
-                    return false;
-                } else {
-                    return true;
-                }
-
-            // case 5.2
-            } else if(Array.isArray(val1) === true && Array.isArray(val2) === true){
-                // array length check
+        // neither is null
+        } else if(val1 !== null && val2 !== null){
+            // initiate a result variable
+            let res = undefined;
+            
+            // both are arrays
+            if(Array.isArray(val1) === true && Array.isArray(val2) === true){
+                // length check
                 if(val1.length !== val2.length){
                     return false;
                 }
+                // recurse each element to compare as primitive
+                for (let i = 0; i < val1.length; i++){
+                    res = deepEqual(val1[i], val2[i]);
+                } return res;
 
-                // call recursive as needed
-                if(recurArr(val1, val2) === false){
+            // both are objects
+            } else if(Array.isArray(val1) === false && Array.isArray(val2) === false){
+                // length check
+                if(Object.keys(val1).length !== Object.keys(val2).length){
                     return false;
-                } else {
-                    return true;
                 }
+                // recurse each property to compare as primitive
+                for (const key in val1){
+                    res = deepEqual(val1[key], val2[key]);
+                } return res;
+            
+
+            // different object types
+            } else{
+                return false;
             }
+
+        // different value types
+        } else{
+            return false;
         }
 
-        // case 5.3
+    // one is a non-primitive
+    } else{
         return false;
     }
-
 }
-
-
-// recursive for nested objects
-function recurObj(val1, val2){
-    // for in loop to compare each property value in an object
-    for (const key in val1){
-        // recurse
-        if(typeof(val1[key]) === 'object'){
-            // check for null as a property value
-            if(val1[key] === null && val2[key] === null){
-                return true;
-            }
-            else if(val1[key] !== null && val2[key] !== null){
-                return recurObj(val1[key], val2[key]);
-            }
-            else{
-                return false;
-            }
-        }
-        // stop recurse
-        else if(val1[key] !== val2[key]){
-            return false;
-        }       
-    }
-}
-
-// recursive for nested arrays
-function recurArr(val1, val2){
-    // for loop to compare each element in an array
-    for (let i = 0; i < val1.length; i++){
-        // recurse
-        if(Array.isArray(val1[i]) === true){
-            // check for null as a property value
-            if(val1[i] === null && val2[i] === null){
-                return true;
-            }
-            else if(val1[i] !== null && val2[i] !== null){
-                return recurArr(val1[i], val2[i]);
-            }
-            else{
-                return false;
-            }
-        }
-        // stop recurse
-        else if(val1[i] !== val2[i]){
-            return false;
-        }       
-    }
-
-}
-
-// test debug
-const majors1 = ['CS', 'Math', [1,2,3], {name: 'ginwook', address: 'corvallis'}];
-const majors2 = ['CS', 'Math', [1,2,3], {name: 'ginwook', address: 'beaverton'}];
-
-// expect true
-console.log(deepEqual(majors1, majors2))
-
-
 
 // Don't add or change anything below this comment.
 module.exports = deepEqual;
