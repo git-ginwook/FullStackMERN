a402f1ec83c00eb8283765f25be089fc0d247e63
git-ginwook <leeginw@oregonstate.edu>
2022-07-10 20:30:38 -0700

    assignment 3 complete

--- assignment_3/server.js
+++ assignment_3/server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 const stocks = require('./stocks.js').stocks;
 
 const express = require("express");
+const { reset } = require('nodemon');
 const app = express();
 
 
@@ -16,11 +17,72 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 // Note: Don't add or change anything above this line.
 
-// Add your code here
+// ONID: leeginw
 
+// match the chosen stock from stocks.js 
+function findStockBySymbol(choice){
+    for(let aStock of stocks){
+        if(aStock.symbol === choice){
+            return aStock
+        }
+    }
+}
 
+// route handler for POST request
+app.post("/review", (req, res) => {
+    // call findStockBySymbol function 
+    const match = findStockBySymbol(req.body.symbol);
 
+    let quantity = parseInt(req.body.quantity);
+    quantity = quantity.toLocaleString();
+    
+    const price = match.price.toLocaleString("en-US", 
+                                            {style:"currency", currency:"USD"});
+    
+    let total = match.price * req.body.quantity;
+    total = total.toLocaleString("en-US",
+                                {style:"currency", currency:"USD"});
+    
+    // HTTP response 
+    res.send(`You ordered ${quantity} stocks of ${match.company}. 
+            The price of one ${match.symbol} is ${price}
+            and, thus, the total price is ${total}.`);
+});
 
+// find stock data that matches the submitted criterion
+function findStockByPrice(click){
+    // initiate result variable
+    let result = stocks[0];
+
+    // loop through stocks array
+    for(let i = 0; i < (stocks.length-1); i++){
+
+        // search for the highest stock price
+        if(click === "highest"){
+            // change only if the next stock has a higher price
+            if(result.price < stocks[i+1].price){
+                result = stocks[i+1];
+            }
+        }
+
+        // search for the lowest stock price
+        if(click === "lowest"){
+            // 
+            if(result.price > stocks[i+1].price){
+                result = stocks[i+1];
+            }
+        }
+    } return result
+};
+
+// route handler for GET request
+app.get("/review", (req, res) => {
+    // call findStockByPrice function
+    const search = findStockByPrice(req.query.criteria);
+    
+    // HTTP response
+    res.send(search);
+});
 
 // Note: Don't add or change anything below this line.
 app.listen(PORT, () => {
