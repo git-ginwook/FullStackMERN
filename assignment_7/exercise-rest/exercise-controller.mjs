4e8eb1465675e6f161dc16d35946b5c58a434dda
ginwooklee_air <leeginw@oregonstate.edu>
2022-08-11 22:24:59 -0700

    fix: UPDATE validation bug fix

--- assignment_7/exercise-rest/exercise-controller.mjs
+++ assignment_7/exercise-rest/exercise-controller.mjs
@@ -78,59 +78,47 @@ app.get('/exercises/:_id', (req, res) => {
 
 
 // UPDATE controller ************************************
-app.put('/exercises/:_id', (req, res) => {
-    exercises.updateExercise(
-        req.params._id, 
-        req.body.name, 
-        req.body.reps, 
-        req.body.weight,
-        req.body.unit,
-        req.body.date
-    )
-    .then(numUpdated => {
-        // validations
-        if (
-            // name validation
-            !req.body.date ||
-            typeof req.body.name !== 'string' ||
-            req.body.name.trim() === '' ||
-            // reps validation
-            !req.body.reps ||
-            typeof req.body.reps !== 'number' || 
-            req.body.reps <= 0 ||
-            // weight validation
-            !req.body.weight ||
-            typeof req.body.weight !== 'number' || 
-            req.body.weight <= 0 ||
-            // unit validation
-            !req.body.unit ||
-            typeof req.body.unit !== 'string' ||
-            !["kgs", "lbs", "miles"].includes(req.body.unit) ||
-            // date validation
-            !req.body.date
-                            
-            ) {
-                // invalid syntax
-                res.status(400).json({ error: 'catch invalid syntax.' });
-        } else if (numUpdated === 1) {
-            res.status(200).json({ 
-                _id: req.params._id, 
-                name: req.body.name, 
-                reps: req.body.reps, 
-                weight: req.body.weight,
-                unit: req.body.unit,
-                date: req.body.date 
-                })
+app.put('/exercises/:_id', [
+    // validations
+    check('name').not().isEmpty().trim().isLength( {min: 0} ).escape(),
+    check('reps').isInt( {gt:0} ),
+    check('weight').isInt( {gt:0} ),
+    check('unit').isIn( ["kgs", "lbs", "miles"] ),
+    check('date').isBefore(Date())
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            return res.status(400).json( {Error: "Invalid request"} )
         } else {
-            res.status(404).json({ Error: 'Document not found' });
+            exercises.updateExercise(
+                req.params._id, 
+                req.body.name, 
+                req.body.reps, 
+                req.body.weight,
+                req.body.unit,
+                req.body.date
+            ).then(numUpdated => {
+                if (numUpdated === 1) {
+                    res.status(200).json({ 
+                        _id: req.params._id, 
+                        name: req.body.name, 
+                        reps: req.body.reps, 
+                        weight: req.body.weight,
+                        unit: req.body.unit,
+                        date: req.body.date 
+                    })
+                } else {
+                    res.status(404).json({ Error: 'Document not found' });
+                }
+            }).catch(error => {
+                console.error(error);
+                res.status(400).json({ Error: 'Request failed' });
+            })
         }
-    })
-    .catch(error => {
-        console.error(error);
-        res.status(400).json({ Error: 'Request failed' });
-    });
-});
+    }
+)
 
 
 // DELETE Controller ******************************
