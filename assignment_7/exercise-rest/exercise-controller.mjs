988406c602295a53237af2002a46efe01521c8ee
ginwooklee_air <leeginw@oregonstate.edu>
2022-08-11 21:20:52 -0700

    base for fixing validation bug

--- assignment_7/exercise-rest/exercise-controller.mjs
+++ assignment_7/exercise-rest/exercise-controller.mjs
@@ -1,53 +1,31 @@
 import 'dotenv/config';
 import express from 'express';
+import { check, validationResult } from 'express-validator'
+
 import * as exercises from './exercise-model.mjs';
+
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
+// const { check, valResult } = require('express-validator')
 
 // CREATE controller ******************************************
 // 1. Create using POST /exercises
-app.post ('/exercises', (req,res) => { 
+app.post ('/exercises', (req, res) => {
     exercises.createExercise(
-        req.body.name, 
-        req.body.reps, 
+        req.body.name,
+        req.body.reps,
         req.body.weight,
         req.body.unit,
         req.body.date
-        )
-        .then(exercise => {
-            // validations
-            if (
-                // name validation
-                typeof req.body.name !== 'string' ||
-                req.body.name.trim() === '' ||
-                // reps validation
-                typeof req.body.reps !== 'number' || 
-                req.body.reps <= 0 ||
-                // weight validation
-                typeof req.body.weight !== 'number' || 
-                req.body.weight <= 0 ||
-                // unit validation
-                typeof req.body.unit !== 'string' ||
-                !["kgs", "lbs", "miles"].includes(req.body.unit)
-                // date validation: covered by the schema attritbute
-
-            ) {
-                // invalid syntax
-                res.status(400).json({ error: 'catch invalid syntax.' });
-            }
-            else {
-                // valid syntax
-                res.status(201).json(exercise);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(400).json({ error: 'Request failed' });
-        });
-});
+    ).then(exercise => {
+        res.status(201).json(exercise);
+    }).catch(error => {
+        res.status(400).json( {Error: 'Invalid request'} )
+    })
+})
 
 
 // READ controllers ****************************************************
